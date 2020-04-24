2975dc280d3fcbbc3972d500e30ade4157880961
D0Mo11 <62610228+D0Mo11@users.noreply.github.com>
2020-04-24 13:37:39 +0200

    Add files via upload

---
+++ RPPOON3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace RPPOON3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dataset first = new Dataset("csv.txt");
+            Dataset second = (Dataset)first.Clone();
+
+            first.Print();
+            second.Print();
+            first.ClearData();
+            first.Print();  // Nece nista ispisati jer je ocisceno
+            second.Print();
+
+        }
+    }
+}
